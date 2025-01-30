b993b8a5b0785351df586cda94ba67e304e4a2e0
CheetoPuff <91032931+Cheet0Puff@users.noreply.github.com>
2025-01-29 20:47:00 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -8,41 +8,41 @@ Section:NewButton("Owners", "@pzuo / @nytmi on discord")
 
 -- blox fruit fuck this shit i hate this
 local bloxf = Window:NewTab("Blox Fruits")
-local bloxs = bloxf:NewSection("all of these scripts are tested by owners & beta testers")
-bloxs:NewButton("set fps for afk", "added by nytmi", function()
+local bloxs = bloxf:NewSection("(All of these scripts are tested by owners & beta testers)")
+bloxs:NewButton("Set fps for afk", "Added by nytmi", function()
     setfpscap(30)
 end)
 
-local bloxkl = bloxf:NewSection("keyless")
+local bloxkl = bloxf:NewSection("Keyless:")
 
-bloxkl:NewButton("frostware", "added by owners", function()
+bloxkl:NewButton("Frostware", "Added by owners", function()
     loadstring(game:HttpGet"https://frostware.onrender.com/fw")()
 end)
 
-bloxkl:NewButton("ganteng", "added by owners", function()
+bloxkl:NewButton("Ganteng", "Added by owners", function()
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a5c3af437cd698d64379cf75cacb9281.lua"))()
 end)
 
-local bloxfk = bloxf:NewSection("with key")
+local bloxfk = bloxf:NewSection("With key:")
 
-bloxfk:NewButton("blacktrap", "added by owners", function()
+bloxfk:NewButton("Blacktrap", "Added by owners", function()
     loadstring(game:HttpGet('https://lua-library.btteam.net/script-auth.txt'))()
 end)
 
-bloxfk:NewButton("alchemy hub (BUGGY ON XENO)", "added by owners", function()
+bloxfk:NewButton("Alchemy Hub (BUGGY ON XENO)", "Added by owners", function()
     loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))()
 end)
 
 local fisc = Window:NewTab("Fisch")
-local fisch = fisc:NewSection("all of these scripts are tested by owners & beta testers")
+local fisch = fisc:NewSection("All of these scripts are tested by owners & beta testers")
 
 local fisch = fisc:NewSection("misc")
-fisch:NewButton("set fps for afk", "added by owners", function()
+fisch:NewButton("Set fps for afk", "Added by owners", function()
     setfpscap(30)
 end)
 local osfisch = fisc:NewSection("open-source")
-osfisch:NewButton("fischy (d)", "added by owners", function()
+osfisch:NewButton("Fischy (d)", "Added by Cheet0Puff", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Cheet0Puff/Fisch-Script-WORKING/refs/heads/main/Fischyscript.lua",true))()
 end)
-osfisch:NewButton("go to info for tut on fischy", "https://github.com/Cheet0Puff/Fisch-Script-WORKING/", function()
+osfisch:NewButton("Go to info for tut on fischy", "https://github.com/Cheet0Puff/Fisch-Script-WORKING/", function()
 end)
