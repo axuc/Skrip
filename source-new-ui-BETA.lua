7fc5ad60db64a4b975576bf17c6ebb53585062e3
axuc <166185199+axuc@users.noreply.github.com>
2025-02-01 00:44:17 -0500

    Update source-new-ui-BETA.lua

--- source-new-ui-BETA.lua
+++ source-new-ui-BETA.lua
@@ -75,3 +75,80 @@ SquidGameRbx:Label("Every script is tested by owners or beta testers")
 SquidGameRbx:Button("nebora.org", "added by nytmi", function()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/499fa8eaaae391c6c3c77f2640811357.lua"))()
 end)
+
+
+
+local combatinit = win:Tab("combatt initiation", "http://www.roblox.com/asset/?id=0")
+combatinit:Label("Every script is tested by owners or beta testers")
+combatinit:Button("press the thing on the bottom left mid wave", "added by nytmi", function()
+local player = game.Players.LocalPlayer
+local Randomize = false
+local enemyPriority = {"Mark", "Beacon"} -- reversed
+local backpack = player:WaitForChild("Backpack")
+local function equipAndActivateTools()
+    local character = player.Character
+    for _, tool in ipairs(backpack:GetChildren()) do
+        if tool:IsA("Tool") then
+            if tool.Parent == backpack then
+                tool.Parent = character
+            end
+        end
+    end
+end
+
+
+if player.PlayerGui:FindFirstChild("SigmaMenu") then
+    player.PlayerGui:FindFirstChild("SigmaMenu"):Destroy()
+end
+local gui = Instance.new("ScreenGui", player.PlayerGui)
+gui.Name = "SigmaMenu"
+local button = Instance.new("ImageButton",gui)
+button.Position = UDim2.new(0,0,0.5,100)
+button.Size = UDim2.new(0,100,0,100)
+button.Image = "rbxassetid://11122932914"
+button.BackgroundColor3 = Color3.new(0,0,0)
+local toggle = false
+local activation = button.Activated:Connect(function()
+    toggle = toggle == false
+    if toggle then
+        button.BackgroundColor3 = Color3.new(1,1,1)
+    else
+        button.BackgroundColor3 = Color3.new(0,0,0)
+    end
+    while toggle do
+        pcall(function()
+            task.wait()
+            local enemyList = {}
+            for _, enemy in workspace.Enemies:GetChildren() do
+                if enemy and enemy.Humanoid and enemy.Humanoid.Health ~= 0 then
+                    table.insert(enemyList, enemy)
+                end
+            end
+            local target
+            if Randomize then
+                target = enemyList[math.random(1, #enemyList)] 
+            else
+               
+                target = enemyList[1] 
+                for _, priority in enemyPriority do
+                    for _, enemy in enemyList do
+                        if enemy.Name == priority then
+                            target = enemy
+                        end
+                    end
+                end
+            end
+          
+            
+            if #enemyList ~= 0 then
+                player.Character.PrimaryPart.CFrame = target.PrimaryPart.CFrame - target.PrimaryPart.CFrame.LookVector * 2 + Vector3.new(0, 1, 0)
+            end
+            equipAndActivateTools()
+        end)
+    end
+end)
+gui.Destroying:Connect(function()
+    activation:Disconnect()
+    toggle = false
+end)
+end)
